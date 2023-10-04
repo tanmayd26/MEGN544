53aa107e1ac06b5dd1897842989c09b2b81dc8fd
tanmayDesai <tade5994@colorado.edu>
2023-10-04 16:40:54 -0600

    HW6_partd

--- HW6/part3a.m
+++ HW6/part3a.m
@@ -13,29 +13,31 @@ theta_vals = cubicinterpolation(a0, a1, a2, a3, t0,t);
 %% Draw Joint Space
 out = VideoWriter('part3a');
 out.open();
-posList = zeros(3,200);
+pos_all = zeros(3,200);
 for i=1:length(t)
 
      T_tmp = forwardkinematics(theta_vals(1,i),theta_vals(2,i),theta_vals(3,i),theta_vals(4,i),theta_vals(5,i),theta_vals(6,i));
-     posList(:,i) = T_tmp(1:3,4);
+     pos_all(:,i) = T_tmp(1:3,4);
      drawArm(theta_vals(1,i),theta_vals(2,i),theta_vals(3,i),theta_vals(4,i),theta_vals(5,i),theta_vals(6,i),1,final_pose);
      pause(1/200);
 end
 out.close();
 
-figure()
-subplot(2,2,[2,4])
+figure(1)
+
  plot(t,theta_vals/pi)
  legend('\theta_1','\theta_2','\theta_3','\theta_4','\theta_5','\theta_6');
  xlabel('Time [s]')
  ylabel('Angle [rad/\pi]')
-subplot(2,2,1)
- plot(tList,posList)
+%subplot(2,2,1)
+figure(2)
+ plot(tList,pos_all)
  legend('X','Y','Z')
  xlabel('Time [s]')
  ylabel('Position [m]')
-subplot(2,2,3)
- plot3(posList(1,:),posList(2,:),posList(3,:))
+%subplot(2,2,3)
+figure(3)
+ plot3(pos_all(1,:),pos_all(2,:),pos_all(3,:))
  xlabel('X [m]')
  ylabel('Y [m]')
  zlabel('Z [m]')
--- HW6/part3b.m
+++ HW6/part3b.m
@@ -9,47 +9,47 @@ R_final=final_pose(1:3,1:3);
 om_init=rot2AngleAxis(R_init);
 %
 om_final=rot2AngleAxis(R_final);
-% Interpolatting Omega
-[a0_om_x,a1_om_x,a2_om_x,a3_om_x] =cubiccoeficient(om_init(1),om_final(1),T);
-[a0_om_y,a1_om_y,a2_om_y,a3_om_y] =cubiccoeficient(om_init(2),om_final(2),T);
-[a0_om_z,a1_om_z,a2_om_z,a3_om_z] =cubiccoeficient(om_init(3),om_final(3),T);
-% Interpolating position
-d0=initial_pose(1:3,4);
-df=final_pose(1:3,4);
-% Interpolate Displacement
-dx=linspace(d0(1),df(1),length(t));
-dy=linspace(d0(2),df(2),length(t));
-dz=linspace(d0(3),df(3),length(t));
-% Interpolate Omega
-for j=1:length(t)
-    om_t_x=a0_om_x+a1_om_x*t(j)+a2_om_x*t(j).^2+a3_om_x*t(j).^3;
-    om_t_y=a0_om_y+a1_om_y*t(j)+a2_om_y*t(j).^2+a3_om_y*t(j).^3;
-    om_t_z=a0_om_z+a1_om_z*t(j)+a2_om_z*t(j).^2+a3_om_z*t(j).^3;
-    om_t=[om_t_x;om_t_y;om_t_z];
-    %
-    R_t=angleAxis2Rot(om_t);
-    d_t=[dx(j);dy(j);dz(j)];
-    T_t=[R_t,d_t;0 0 0 1];
-    joints=inversekinematics(T_t,);
-    Joint_Variable(j,:,2)=joints;
+
+d_init = initial_pose(1:3,4);
+d_final = final_pose(1:3,4);
+
+pos_om_init = [d_init; om_init];
+pos_om_final = [d_init; om_init];
+
+initial_pose_inv = inversekinematics(initial_pose,zeros(6,1));
+[a0, a1, a2, a3] = cubiccoeficient(pos_om_init,pos_om_final,T);
+pos_all = cubicinterpolation(a0,a1,a2,a3,t0,t);
+theta_vals = zeros(6,length(t));
+for i=1:length(t)
+    theta = norm(pos_all(4:6,i));
+    k_vec = pos_all(4:6,i)/theta;
+    Trans = [angleAxis2Rot(k_vec*theta) pos_all(1:3,i); 0 0 0 1];
+    if i>1
+        joint = inversekinematics(Trans,theta_vals(:,i-1));
+    else
+        joint = inversekinematics(Trans,[initial_pose_inv(6,1)]);
+    end
+        theta_vals(:,i) = transpose(joint);
+        drawArm(theta_vals(1,i), theta_vals(2,i), theta_vals(3,i), theta_vals(4,i), theta_vals(5,i), theta_vals(6,i))
 end
+
 
 %
-out = VideoWriter('part3b');
-out.open();
-
-for i=1:length(t)
-    for j=1:4
-
-     T_tmp = Joint_Variable(i,:,j);
-     drawArm( T_tmp );
-     hold on;
-
-% What Ever Else You Want To Plot...
-
-    hold off;
-    out.writeVideo(getframe(H));
-    pause(1/N);
-    end
-end
-out.close();
+% out = VideoWriter('part3b');
+% out.open();
+% 
+% for i=1:length(t)
+% 
+% 
+% 
+%      drawArm( T_tmp );
+%      hold on;
+% 
+% % What Ever Else You Want To Plot...
+% 
+%     hold off;
+%     out.writeVideo(getframe(H));
+%     pause(1/N);
+%     end
+% end
+% out.close();
